f5e79b7246c383fc11a5ead7a0a0a347a2da822f
B3none <ablackham2000@gmail.com>
2019-08-09 10:52:09 +0100

    Add initial Client

---
+++ src/Client.php
@@ -0,0 +1,19 @@
+<?php
+
+
+class Client
+{
+    /**
+     * Create an instance of the client.
+     *
+     * @return Client
+     */
+    public static function create()
+    {
+        return new self();
+    }
+
+    public function __construct()
+    {
+    }
+}
