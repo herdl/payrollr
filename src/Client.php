45281610a1be419dbe5ece0e0824cde45d962a99
B3none <ablackham2000@gmail.com>
2019-08-09 13:16:23 +0100

    Implement GuzzleHelper

--- src/Client.php
+++ src/Client.php
@@ -2,19 +2,18 @@
 
 namespace B3none\PayRun;
 
+use B3none\PayRun\Helpers\GuzzleHelper;
+use GuzzleHttp\Client as GuzzleClient;
+
 class Client
 {
     /**
-     * Create an instance of the client.
-     *
-     * @return Client
+     * @var GuzzleClient
      */
-    public static function create()
-    {
-        return new self();
-    }
+    protected $guzzleClient;
 
-    public function __construct()
+    public function __construct($consumerKey, $consumerSecret, $isTesting = false)
     {
+        $this->guzzleClient = GuzzleHelper::create($consumerKey, $consumerSecret, $isTesting);
     }
 }
---
+++ src/Helpers/GuzzleHelper.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace B3none\PayRun\Helpers;
+
+use GuzzleHttp\Client;
+use GuzzleHttp\HandlerStack;
+use GuzzleHttp\Subscriber\Oauth\Oauth1;
+
+class GuzzleHelper
+{
+    const BASE_URL = 'https://api.payrun.io';
+    const TEST_URL = 'https://api.test.payrun.io';
+
+    /**
+     * RequestHelper constructor.
+     *
+     * @param string $consumerKey
+     * @param string $consumerSecret
+     * @param bool $isTest
+     * @return Client
+     */
+    public static function create(string $consumerKey, string $consumerSecret, bool $isTest)
+    {
+        $stack = HandlerStack::create();
+
+        $middleware = new Oauth1([
+            'consumer_key' => $consumerKey,
+            'consumer_secret' => $consumerSecret,
+        ]);
+        $stack->push($middleware);
+
+        return new Client([
+            'base_uri' => $isTest ? self::TEST_URL : self::BASE_URL,
+            'handler' => $stack,
+            'auth' => 'oauth',
+        ]);
+    }
+}
