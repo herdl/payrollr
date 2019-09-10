c71d009adb0aaae8140edb5820fb3d5b7ca9dabf
B3none <ablackham2000@gmail.com>
2019-09-10 11:43:37 +0100

    Add HealthCheckModel

--- src/Models/HealthCheckModel.php
+++ src/Models/HealthCheckModel.php
@@ -4,5 +4,53 @@ namespace B3none\PayRun\Models;
 
 class HealthCheckModel
 {
+    /**
+     * The current version number of the API.
+     *
+     * @var string
+     */
+    protected $version;
 
+    /**
+     * Further detailed information about the health of the API.
+     *
+     * @var string
+     */
+    protected $info;
+
+    /**
+     * @param string $version
+     * @return HealthCheckModel
+     */
+    public function setVersion(string $version): HealthCheckModel
+    {
+        $this->version = $version;
+        return $this;
+    }
+
+    /**
+     * @param string $info
+     * @return HealthCheckModel
+     */
+    public function setInfo(string $info): HealthCheckModel
+    {
+        $this->info = $info;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getVersion(): string
+    {
+        return $this->version;
+    }
+
+    /**
+     * @return string
+     */
+    public function getInfo(): string
+    {
+        return $this->info;
+    }
 }
