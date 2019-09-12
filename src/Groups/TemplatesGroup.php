271bf6eff756e2975978764c6a3c974b3c659050
B3none <ablackham2000@gmail.com>
2019-09-12 14:58:54 +0100

    Add Templates endpoints

--- src/Groups/TemplatesGroup.php
+++ src/Groups/TemplatesGroup.php
@@ -4,5 +4,36 @@ namespace B3none\PayRun\Groups;
 
 class TemplatesGroup extends BaseGroup
 {
+    /**
+     * https://developer.payrun.io/explore/#!/Templates/GetTemplate
+     *
+     * Returns a template instance of the specified data type
+     *
+     * @param string $dtoDataType
+     * @return array
+     */
+    public function getTemplate(string $dtoDataType): array
+    {
+        $response = $this->guzzleClient->get(
+            "/Template/{$dtoDataType}"
+        );
 
+        return $this->getResponseData($response);
+    }
+
+    /**
+     * https://developer.payrun.io/explore/#!/Templates/GetTemplates
+     *
+     * Returns a collection of links to all the available data object templates
+     *
+     * @return array
+     */
+    public function getTemplates(): array
+    {
+        $response = $this->guzzleClient->get(
+            "/Templates"
+        );
+
+        return $this->getResponseData($response);
+    }
 }
