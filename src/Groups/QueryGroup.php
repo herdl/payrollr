d26f74ed3e2158926fe9769ac47e77b00d4f8ba0
B3none <ablackham2000@gmail.com>
2019-09-12 13:31:58 +0100

    Added Query endpoint

--- src/Groups/QueryGroup.php
+++ src/Groups/QueryGroup.php
@@ -4,5 +4,25 @@ namespace B3none\PayRun\Groups;
 
 class QueryGroup extends BaseGroup
 {
+    /**
+     * https://developer.payrun.io/explore/#!/Query/GetQueryResponse
+     *
+     * Deletes the specified pension revision for the matching revision number
+     *
+     * @param array $query
+     * @return array
+     */
+    public function getQueryResponse(array $query): array
+    {
+        $response = $this->guzzleClient->post(
+            "/Query",
+            [
+                'body' => [
+                    'Query' => $query
+                ]
+            ]
+        );
 
+        return $this->getResponseData($response);
+    }
 }
