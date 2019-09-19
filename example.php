a74762e4abf69c41e881b0677236d5d8fcb3d56a
B3none <ablackham2000@gmail.com>
2019-09-19 11:04:37 +0100

    Continued with the project rename

--- example.php
+++ example.php
@@ -1,14 +1,9 @@
 <?php
 
 use Herdl\PayRun\Client;
-use Herdl\PayRun\Models\EmployerModel;
 
 require('vendor/autoload.php');
 
-$payRun = new Client('sQzgBcZkiku8ZlMLx75mEQ', 'qE299Jo2fEGKoDSbJSLQZ2QlTDql5kOPXZzRKhtKUQ', true);
+$payRun = new Client('CONSUMER_KEY', 'PRIVATE_KEY', true);
 
-$employer = new EmployerModel();
-
-$employer->setEffectiveDate(time());
-
-print_r($payRun->employer()->postEmployer($employer->format()));
+print_r($payRun->healthCheck()->getHealthCheck());
